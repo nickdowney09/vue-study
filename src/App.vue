30a88f87f40047d6883ade86fca28c788c94fe57
tangyiling <tangyiling@chuchujie.com>
2018-03-12 09:33:30 +0800

    todo组件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
-<div>
-    <h1>TEST</h1>
-    <div>{{ message }}</div>
-</div>
+    <div>
+        <input type="text" v-model="newTodo" @keyup.enter="addNewTodo" placeholder="add new Todo">
+        <ul>
+            <li 
+                is="todo-item" 
+                v-for="(item, index) in todos" 
+                :key="item.id"
+                :title="item.title"
+                @remove="todos.splice(index, 1)"
+            ></li>
+        </ul>
+    </div>
 </template>
 
 <script>
+import todoItem from '@/components/todo_item';
+
 export default {
   name: 'app',
   data: () => {
       return {
-          message: 'This is Life'
+          message: 'This is Life',
+          newTodo: '',
+          todos: [{
+              id: 1,
+              title: '总结webpack配置'
+          }],
+          nextTodoId: 2
+      };
+  },
+  components: {
+      todoItem
+  },
+  methods: {
+      addNewTodo:function() {
+          this.todos.push({
+              title: this.newTodo,
+              id: this.nextTodoId++
+          });
+          this.newTodo = '';
       }
   }
-}
+};
 </script>
---
+++ src/components/todo_item.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <span>{{ title }}</span>
+        <button @click="$emit('remove')">X</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'todo-item',
+    props: ['title']
+};
+</script>
